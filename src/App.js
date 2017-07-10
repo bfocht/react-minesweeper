119e492eba75eee28be35c94a10726f695accb1b
Bryan Focht <bxfocht@godaddy.com>
2017-07-10 13:22:38 -0700

    update requirements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Minefield from './Minefield';
 import logo from './logo.svg';
 import './App.css';
 
+/** Notes **********************************************************************
+*  Minesweeper is a single-player puzzle video game. The objective of the
+*  game is to clear a rectangular board (minefield) containing hidden "mines" or
+*  bombs without detonating any of them, with help from clues about the number of
+*  neighboring mines in each field.
+*  https://en.wikipedia.org/wiki/Minesweeper_(video_game)
+*
+*  This is the main application.   You should not need to modify this component
+*  as it renders the Minefield component.  The requirements of the game are in
+*  the src/Minefield source.
+*
+*********************************************************************************/
+
 class App extends Component {
   constructor(props) {
     super(props);
--- src/Minefield.js
+++ src/Minefield.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// Use this component to build the minefield game
-// 1. It should return a grid based on the rows/cols props in the render function.
-// 2. Each cell should handle a click event
-// 3. If the cell contains a mine, then the player should be notified they lost
-// 4. If the cell is adjacent to one or more mines, the the player should be notified how many mines are nearby
-// 5. If the cell is is not adjacent to a mine, then the grid should clear until and cell to an adjacent mine is found.
-// 6. If all cells except the mines are cleared then the player should be notified they won.
-// 7. Bonus: right clicking on a field should set a flag
+/** Requirements ***************************************************************
+*  Use this component to build the minefield game using the three props given
+*  (rows, cols, mines)
+*
+*  1. This component should return a grid based on the rows/colums.
+*    * The number of rows and cols are passed in as Props from src/App.js
+*    * Some CSS has already been provided in src/App.css
+*    * The row can be a <div class="row"> </div>
+*    * In each row you can specify a <span class="cell"> </span>
+*
+*  2. The content of each  cell should not be visible until the player clicks on the cell.
+*    * Each cell should handle a OnClick event
+*    * removing the `covered` class will make the content of cell visible
+*
+*  3. Mines should be randomly placed in the minefield based on the `mines` prop
+*    * Mines should be generated on inital page render and when the player changes a prop
+*    * react provides lifecycle methods to help with this
+*    * https://facebook.github.io/react/docs/react-component.html
+*
+*  4. If the cell contains a mine, then the player should be notified they lost
+*    * A mine can be denoted with any character such as `@`
+*    * A simple alert will suffice
+*
+*  5. If the cell is adjacent to one or more mines, the the player should be notified how many mines are nearby
+*  6. If the cell is is not adjacent to a mine, then the grid should clear until and cell to an adjacent mine is found.
+*  7. If all cells except the mines are cleared then the player should be notified they won.
+*  8. Right clicking on a field should set a flag
+********************************************************************************/
 
 class Minefield extends Component {
   render() {
